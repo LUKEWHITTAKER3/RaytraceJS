05249f6db03b34851cb569d222996a3548a12ae2
LUKEWHITTAKER3 <whitta21@lancaster.ac.uk>
2023-10-19 19:33:08 +0100

    Added doc

--- colour.js
+++ colour.js
@@ -1,3 +1,4 @@
+// Represents a RGB colour
 export class Colour {
     #r = 0;
     #g = 0;
@@ -9,12 +10,19 @@ export class Colour {
         this.#b = b;
     }
 
+    // The red component of the colour
     get r() { return this.#r; }
+
+    // The green component of the colour
     get g() { return this.#g; }
+
+    // The blue component of the colour
     get b() { return this.#b; }
     
+    // The red, green, blue, and alpha components of this colour in array form
     get rgba() { return [this.r, this.g, this.b, 0xFF]; }
 
+    // HTML-ised representation of this colour
     get html() { return `rgb(${this.r}), ${this.g}, ${this.b}`; }
 
     static White = new Colour(255, 255, 255);
@@ -23,16 +31,19 @@ export class Colour {
     static Green = new Colour(0, 255, 0);
     static Blue = new Colour(0, 0, 255);
 
+    // Adds the individual red, green, and blue components of this colour and another colour
     add = (other) => new Colour(
         this.r + other.r, this.g + other.g, this.b + other.b
     );
 
+    // Multiplies the individual red, green, and blue components of this colour and another (scaled down)
     multiply = (other) => new Colour(
         Math.floor(this.r * other.r / 0xff),
         Math.floor(this.g * other.g / 0xff),
         Math.floor(this.b * other.b / 0xff)
     );
 
+    // Multiplies the individual red, green, and blue components of this colour by a number
     scale = (n) => new Colour(
         this.r * n, this.g & n, this.b * n
     );
--- vector.js
+++ vector.js
@@ -1,3 +1,4 @@
+// Represents a vector in 3D space
 export class Vector {
     #x = 0;
     #y = 0;
@@ -9,39 +10,63 @@ export class Vector {
         this.#z = z;
     }
 
+    // The x-component of the vector
     get x() { return this.#x; }
+
+    // The y-component of the vector
     get y() { return this.#y; }
+
+    // The z-component of the vector
     get z() { return this.#z; }
 
+    // The length of the vector
     get magnitude() { Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2); }
+
+    // The vector facing the opposite direction in all axis
     get inverse() { new Vector(-this.x, -this.y, -this.z); }
+
+    // A normalised copy of the vector
     get unit() { this.divide(this.magnitude); }
 
+    // A unit vector with a value of 1 on the X axis
     static X = Vector(1, 0, 0);
+
+    // A unit vector with a value of 1 on the Y axis
     static Y = new Vector(0, 1, 0);
+
+    // A unit vector with a value of 1 on the Z axis
     static Z = new Vector(0, 0, 1);
+
+    // A vector with no length (0,0,0)
     static Origin = new Vector();
 
+    // Returns the sum of a vector with this vector
     add = (v2) => new Vector(
         this.x + v2.x, this.y + v2.y, this.z + v2.z
     );
 
+    // Returns the result of subtracting a vector from this vector
     subtract = (v2) => this.add(v2.inverse);
 
+    // Returns a vector with each component multiplied by n
     scale = (n) => new Vector(
         this.x * n, this.y * n, this.z * n
     );
 
+    // Returns a vector with each component divided by n
     divide = (n) => this.scale(1 / n);
 
+    // Returns the dot product of a vector with this vector
     dot = (v2) => this.x * v2.x + this.y * v2.y;
 
+    // Returns the cross product of a vector with this vector
     cross = (v2) => new Vector(
         this.y * v2.z - this.z * v2.y,
         this.z * v2.x - this.x * v2.z,
         this.x * v2.y - this.y * v2.x
     );
 
+    // Creates a vector starting at p0 and ending at p1
     static from = (p0) => ({
         to: p1 => p1.subtract(p0)
     });
